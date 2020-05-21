f423e3c8ed6f3662f2f6ab79f26c80476044a83e
SnorriArinbjarnar <snorriarinbjarnar@gmail.com>
2020-05-21 19:01:54 +0000

    added tests for email

--- emailassistant/EmailWizard/tests/test_models.py
+++ emailassistant/EmailWizard/tests/test_models.py
@@ -3,7 +3,6 @@ from django.test import TestCase
 from ..models import Email, EmailGroup 
 
 class EmailGroupTest(TestCase):
-
     def test_string_representation(self):
         # Making sure we have changed __str__
         # so it gives somethinb better than Object object 
@@ -13,3 +12,15 @@ class EmailGroupTest(TestCase):
     def test_get_absolute_url(self):
         group = EmailGroup.objects.create(title='Initial Title')
         self.assertIsNotNone(group.get_absolute_url())
+
+class CompanyEmailTest(TestCase):
+    def setUp(self):
+        # connect with an emailgroup
+        self.emailgroup = EmailGroup.objects.create(
+            title='test-emailgroup'
+        )
+        
+    def test_string_representation(self):
+        email = Email(company="Arcana", email="arcana@arcana.is", emailgroup=self.emailgroup)
+        self.assertEqual(str(email), "Arcana:arcana@arcana.is")
+    
--- emailassistant/EmailWizard/tests/test_views.py
+++ emailassistant/EmailWizard/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase 
-from EmailWizard.models import EmailGroup
+from EmailWizard.models import EmailGroup, Email
+from django.urls import reverse
 
 class HomeViewTestCase(TestCase):
     """ Test wether email groups show up on page """
@@ -11,7 +12,7 @@ class HomeViewTestCase(TestCase):
         response = self.client.get('/emailForMe', follow=True)
         self.assertContains(response, '1-title')
 
-    def test_one_emailgroup(self):
+    def test_two_emailgroups(self):
         EmailGroup.objects.create(title='1-title')
         EmailGroup.objects.create(title='2-title')
 
@@ -19,7 +20,7 @@ class HomeViewTestCase(TestCase):
         self.assertContains(response, '1-title')
         self.assertContains(response, '2-title')
 
-class GroupViewTest(TestCase):
+class GroupListTest(TestCase):
     def setUp(self):
         self.group = EmailGroup.objects.create(title='1-title')
     def test_basic_view(self):
@@ -28,3 +29,21 @@ class GroupViewTest(TestCase):
     def test_title(self):
         response = self.client.get(self.group.get_absolute_url())
         self.assertContains(response, self.group.title)
+
+class GroupDetailTest(TestCase):
+    def setUp(self):
+        self.group = EmailGroup.objects.create(title='1-title')
+       # self.response = self.client.get(self.group.get_absolute_url(), follow=True)
+        self.response = self.client.get(reverse('group_detail', args=(self.group.id,)))
+    def test_no_emails(self):
+        self.assertContains(self.response, 'No companies added yet')
+    def test_one_email(self):
+        email = Email(company="Arcana", email="arcana@arcana.is", emailgroup=self.group)
+        email.save()
+        response = self.client.get(reverse('group_detail', args=(self.group.id,)))
+        self.assertContains(response, email.company + ':' + email.email)
+
+
+    
+
+
