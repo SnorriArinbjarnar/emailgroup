fa0e91131e2dc44d909fd3fa769e7ff3bd723730
SnorriArinbjarnar <snorriarinbjarnar@gmail.com>
2020-06-27 22:23:47 +0000

    you can add companies to emailgroup, needs refactoring

---
+++ emailassistant/EmailWizard/forms.py
@@ -0,0 +1,23 @@
+
+from django import forms 
+from EmailWizard.models import EmailGroup, Email
+
+class AddCompanyForm(forms.Form):
+    companyName = forms.CharField(max_length=100, label='Company Name:')
+    companyEmail = forms.EmailField(required=True, label='Company Email Address:')
+
+class AddCompanyFormTwo(forms.ModelForm):
+    class Meta:
+        model = Email 
+        fields = ['company', 'email', 'emailgroup']
+
+        def __init__(self, *args, **kwargs):
+            self.emailgroup = kwargs.pop('pk')
+            #
+            super(AddCompanyFormTwo, self).__init__(*args, **kwargs)
+            self.fields['emailgroup'] = self.emailgroup
+
+        def save(self, commit=True):
+            instance = super(AddCompanyFormTwo, self).save(commit=False)
+            instance.save()
+            return instance
--- emailassistant/EmailWizard/views.py
+++ emailassistant/EmailWizard/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView 
+from django.views.generic.edit import FormView, FormMixin
+from EmailWizard.forms import AddCompanyForm, AddCompanyFormTwo
 from EmailWizard.models import EmailGroup, Email
 
 # Create your views here.
@@ -10,7 +13,36 @@ class HomeView(ListView):
     queryset = EmailGroup.objects.order_by('-created_at')
     context_object_name = 'emailgroups'
 
-class GroupDetailView(DetailView):
+class GroupDetailView(FormMixin, DetailView):
     model = EmailGroup
     template_name = 'emailwizard/group_detail.html'
+    form_class = AddCompanyFormTwo
+
+    #def get(self, request, pk):
+        #return request.id
+    # Stay on the same page when successful
+    def get_success_url(self):
+        return reverse('group_detail', kwargs={'pk': self.object.pk})
+
+    # 
+    #def get_context_data(self, **kwargs):
+        #context =  super(GroupDetailView, self).get_context_data(**kwargs)
+        #context['form'] = AddCompanyForm(initial={'post' : self.object})
+        #return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+    
+    def form_valid(self, form):
+        #form.save()
+        #return super(GroupDetailView, self).form_valid(form)
+        #form.save()
+        instance = form.save(commit=False)
+        instance.save()
+        return super().form_valid(form)
     
